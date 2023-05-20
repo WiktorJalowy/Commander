8ac1b8efe4fb57d5ee63f38b2231585df222300d
Witas <wiktor.jalowy@o2.pl>
2023-05-20 15:46:01 +0200

    Changed one test

--- lib/tests/CommanderTests.cpp
+++ lib/tests/CommanderTests.cpp
@@ -40,8 +40,8 @@ TEST(CommmanderTests, ShouldMoveToMine)
 TEST(CommmanderTests, ShouldMoveToTheClosestFieldToMine)
 {
     Commander commander;
-    commander.GiveOrders(std::stringstream("00060 00009 00000"), std::stringstream("400 P B 0 0 0 200 0 E B 1 4 2 200 A P W 2 0 0 80"));
+    commander.GiveOrders(std::stringstream("00960 00009 00000"), std::stringstream("400 P B 0 0 0 200 0 E B 1 4 2 200 A P W 2 0 0 80"));
     std::string str = commander.GetElement(1);
-    EXPECT_EQ(str[4], '2');
-    EXPECT_EQ(str[6], '0');
+    EXPECT_EQ(str[4], '1');
+    EXPECT_EQ(str[6], '1');
 }
